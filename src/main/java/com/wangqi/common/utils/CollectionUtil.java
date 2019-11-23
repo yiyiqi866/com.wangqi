19f60d52e685f1b7ae3dc1212301711e2be16814
yizhongren <email@example.com>
2019-11-23 11:50:17 +0800

    七七七

---
+++ src/main/java/com/wangqi/common/utils/CollectionUtil.java
@@ -0,0 +1,17 @@
+package com.wangqi.common.utils;
+
+import java.util.Collection;
+
+public class CollectionUtil {
+	
+	public static boolean isEmpty(Collection<?> src) {
+		
+		
+		if(null==src || src.size()==0) {
+		    return true;
+		}
+		return false;
+		
+	}
+
+}
